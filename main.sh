0d83b90142a682ff1174de575132366a17311201
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-04 21:20:25 +0300

    manually patch

--- main.sh
+++ main.sh
@@ -1,15 +1,17 @@
+#! /bin/bash
 # Add dependent repositories
 wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
 add-apt-repository https://ppa.pika-os.com
 add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
-tar -xf ./obs-studio_29.0.2.orig.tar.gz -C ./
+tar -xf ./obs-studio_29.0.2.tar.gz -C ./
 tar -xf ./cef_binary_5060_linux64.tar.xz -C ./build_dependencies/
 mv ./obs-studio-0fb8bb4b1e18ee1c870c48d35ab5b598af3b59e9 ./obs-studio
 cp -rvf ./debian ./obs-studio/
 cp -rvf ./build_dependencies  ./obs-studio/
 cd ./obs-studio
+for i in ../patches/*; do patch -Np1 -i $i ;done
 
 # Get build deps
 apt-get build-dep ./ -y
