e1f679954bdd20d78d09ad925b3cc325e425435e
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-04 20:45:23 +0300

    fix orig #2

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ add-apt-repository https://ppa.pika-os.com
 add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
-tar -xf ./obs-studio-29.0.2.orig.tar.gz -C ./
+tar -xf ./obs-studio_29.0.2.orig.tar.gz -C ./
 tar -xf ./cef_binary_5060_linux64.tar.xz -C ./build_dependencies/
 mv ./obs-studio-0fb8bb4b1e18ee1c870c48d35ab5b598af3b59e9 ./obs-studio
 cp -rvf ./debian ./obs-studio/
