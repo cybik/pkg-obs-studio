a884d791fca4d4c1faa47c4aa663887ac3031b35
ferrreo <harderthanfire@gmail.com>
2023-04-28 20:32:02 +0100

    Add missing plugins

--- main.sh
+++ main.sh
@@ -13,6 +13,18 @@ cd ./obs-studio
 
 for i in ../patches/*.patch; do patch -Np1 -i $i ;done
 
+# Prepare plugins/obs-vkcapture
+git clone --recurse-submodules https://github.com/nowrep/obs-vkcapture plugins/obs-vkcapture
+cd plugins/obs-vkcapture
+sed -i 's/install_obs_plugin_with_data/setup_plugin_target/g' CMakeLists.txt
+cd ../../
+
+# Prepare plugins/obs-source-record
+git clone --recurse-submodules https://github.com/exeldro/obs-source-record plugins/obs-source-record
+cd plugins/obs-source-record
+sed -i 's/install_obs_plugin_with_data/setup_plugin_target/g' CMakeLists.txt
+cd ../../
+
 # Get build deps brute force
 apt-get install -y cmake ninja-build pkg-config clang clang-format build-essential curl ccache git libffmpeg-amf-dev
 apt-get install -y libavcodec-dev libavdevice-dev libnss3-dev libnspr4-dev libpipewire-0.3-dev libavfilter-dev libavformat-dev libavutil-dev libswresample-dev libswscale-dev libx264-dev libcurl4-openssl-dev libmbedtls-dev libgl1-mesa-dev libjansson-dev libluajit-5.1-dev python3-dev libx11-dev libxcb-randr0-dev libxcb-shm0-dev libxcb-xinerama0-dev libxcb-composite0-dev libxcomposite-dev libxinerama-dev libxcb1-dev libx11-xcb-dev libxcb-xfixes0-dev swig libcmocka-dev libxss-dev libglvnd-dev libgles2-mesa libgles2-mesa-dev libwayland-dev librist-dev libsrt-openssl-dev libpci-dev
