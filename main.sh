ef3ff6c5df09806aad22f20c36df22b66de0c56d
ferrreo <harderthanfire@gmail.com>
2023-04-28 18:26:48 +0100

    oh god why is obs such a pain

--- main.sh
+++ main.sh
@@ -7,22 +7,34 @@ add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone --recursive https://github.com/obsproject/obs-studio.git
-#wget https://cdn-fastly.obsproject.com/downloads/cef_binary_5060_linux64.tar.bz2
-#mkdir -p ./build_dependencies/
-#tar -xf ./cef_binary_5060_linux64.tar.bz2 -C ./build_dependencies/
-#cp -rvf ./debian ./obs-studio/
-#cp -rvf ./build_dependencies  ./obs-studio/
+wget https://cdn-fastly.obsproject.com/downloads/cef_binary_5060_linux64.tar.bz2
+mkdir -p ./build_dependencies/
+tar -xf ./cef_binary_5060_linux64.tar.bz2 -C ./build_dependencies/
+cp -rvf ./debian ./obs-studio/
+cp -rvf ./build_dependencies  ./obs-studio/
 cd ./obs-studio
 
 #for i in ../patches/*; do patch -Np1 -i $i ;done
 
-# Get build deps
-apt-get build-dep ./ -y
+# Get build deps brute force
+#apt-get build-dep ./ -y
+apt-get install cmake ninja-build pkg-config clang clang-format build-essential curl ccache git
+apt-get install libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libswresample-dev libswscale-dev libx264-dev libcurl4-openssl-dev libmbedtls-dev libgl1-mesa-dev libjansson-dev libluajit-5.1-dev python3-dev libx11-dev libxcb-randr0-dev libxcb-shm0-dev libxcb-xinerama0-dev libxcb-composite0-dev libxcomposite-dev libxinerama-dev libxcb1-dev libx11-xcb-dev libxcb-xfixes0-dev swig libcmocka-dev libxss-dev libglvnd-dev libgles2-mesa libgles2-mesa-dev libwayland-dev librist-dev libsrt-openssl-dev libpci-dev
+apt-get install qt6-base-dev qt6-base-private-dev libqt6svg6-dev qt6-wayland qt6-image-formats-plugins
+apt-get install libasound2-dev libfdk-aac-dev libfontconfig-dev libfreetype6-dev libjack-jackd2-dev libpulse-dev libsndio-dev libspeexdsp-dev libudev-dev libv4l-dev libva-dev libvlc-dev libdrm-dev
 
-./CI/build-linux.sh
+cmake -S . -B ./.build -G Ninja \
+    -DCEF_ROOT_DIR="../obs-build-dependencies/cef_binary_5060_linux64" \
+    -DENABLE_PIPEWIRE=ON \
+    -DENABLE_AJA=0
+
+cmake --build ./.build --target package
+
+
+#./CI/build-linux.sh
 
 # Build package
-dpkg-buildpackage
+#dpkg-buildpackage
 
 # Move the debs to output
 cd ../
