5db0e95ed7d4ca7cce2274e30e6d107565bac929
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-06 22:22:02 +0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,8 +1,5 @@
-# send debs to server
-rsync -azP --include './' --include '*.deb' --exclude '*' ./output/ ferreo@direct.pika-os.com:/srv/www/incoming/
-
 # add debs to repo
-ssh ferreo@direct.pika-os.com 'aptly repo add -force-replace -remove-files pikauwu-main /srv/www/incoming/'
-
+ssh ferreo@direct.pika-os.com 'aptly repo remove pikauwu-main libwebrtc-audio-processing-dev_1.3.0-100pika1_amd64.deb'
+ssh ferreo@direct.pika-os.com 'aptly repo remove pikauwu-main libwebrtc-audio-processing1_1.3.0-100pika1_amd64.deb'
 # publish the repo
 ssh ferreo@direct.pika-os.com 'aptly publish update -batch -skip-contents -force-overwrite pikauwu filesystem:pikarepo:'
